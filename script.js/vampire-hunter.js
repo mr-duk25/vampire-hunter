6b3d6338d957a0bfadcc07979de76b1b8c704ea5
mr-duk25 <samuelrobles@samuels-mbp.mynetworksettings.com>
2024-01-03 22:35:12 -0800

    moved code into update fxn

--- script.js/vampire-hunter.js
+++ script.js/vampire-hunter.js
@@ -6,12 +6,27 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ['stick'];
+const locations = [
+    {
+        name: 'town square',
+        'button text': ['Visit the blacksmith', 'visit the cave', 'Fight the Master Vampire'],
+        'button functions': [visitBlacksmith, visitCave, fightMasterVamp],
+        text1: "You are in the town square. You see a sign that says \"Blacksmith\"."
+    },
+    {
+        name: 'store',
+        'button text': ['Buy 10 health (10 gold)', 'Buy weapon (30 gold)', 'Return to town square'],
+        'button functions': [buyHealth, buyWeapon, goTown],
+        text1: 'you enter the store.'
+    }
+];
 
 // DOM elements
 const button1 = document.querySelector('#button1')
 const button2 = document.querySelector('#button2')
 const button3 = document.querySelector('#button3')
-const text = document.querySelector('#text')
+const text1 = document.querySelector('#textboxone')
+const text2 = document.querySelector('#textboxone')
 const xpText = document.querySelector('#xpText')
 const healthText = document.querySelector('#healthText')
 const goldText = document.querySelector('#goldText')
@@ -25,6 +40,9 @@ button2.onclick = visitCave;
 button3.onclick = fightMasterVamp;
 
 // Functions
+function update(location) {
+
+}
 function goTown() {
 button1.innerText = 'Visit the Blacksmith';
 button2.innerText = 'Visit the Cave';
@@ -32,7 +50,7 @@ button3.innerText = 'Fight the Master Vampire';
 button1.onclick = visitBlacksmith;
 button2.onclick = visitCave;
 button3.onclick = fightMasterVamp;
-text.innerText = "You are in the town square. You see a sign that says \"Blacksmith\"."
+text1.innerText = "You are in the town square. You see a sign that says \"Blacksmith\".";
 }
 
 function visitBlacksmith() {
@@ -42,7 +60,8 @@ button3.innerText = 'Return to town square';
 button1.onclick = buyHealth;
 button2.onclick = buyWeapon;
 button3.onclick = goTown;
-text.innerText = "Blacksmith Cyrus: \"Welcome to the shop, What'll it be\"."
+text1.innerText = "You enter the store"
+
 }
 function visitCave() {
 console.log('Going to Cave.')
@@ -57,4 +76,5 @@ console.log('+10 HP!')
 function buyWeapon() {
 console.log('Blacksmith Cyrus: fresh off the iron')
 }
+
 
